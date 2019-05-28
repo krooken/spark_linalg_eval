7410475c314a5b2f39daf92f86caec54b3e9290b
Hannes Eriksson <hannese@chalmers.se>
2019-05-28 13:27:40 +0200

    lala

--- hannes_test.py
+++ hannes_test.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 
 mat = scipy.io.mmread('rw136.mtx')
+mat2 = scipy.io.mmread('rw136.mtx')
 
 sc = pyspark.context.SparkContext.getOrCreate()
 
@@ -24,7 +25,7 @@ rowmatrix.computePrincipalComponents(3)
 
 end = time.time()
 delta = end-start
-print "PCA(3) took: ", delta, " seconds."
+print "PCA(3)  took: ", delta, " seconds, for matrix: rw136".
 
 start = time.time()
 
@@ -37,6 +38,59 @@ rowmatrix.computeCovariance()
 
 end = time.time()
 delta = end-start
-print "Cov took: ", delta, " seconds."
+print "Cov took: ", delta, " seconds, for matrix: rw136".
 
+start = time.time()
 
+for row in range(rows):
+    data_row = mat.getrow(row)
+    data_rows = data_rows + [pyspark.mllib.linalg.Vectors.sparse(cols, zip(data_row.indices, data_row.data))]
+rdd_rows = sc.parallelize(data_rows)
+rowmatrix = pyspark.mllib.linalg.distributed.RowMatrix(rdd_rows)
+rowmatrix.computeSVD(3)
+
+end = time.time()
+delta = end-start
+print "SVD(3) took: ", delta, " seconds, for matrix: rw136".
+
+(rows, cols) = mat2.shape
+data_rows = []
+
+start = time.time()
+
+for row in range(rows):
+    data_row = mat2.getrow(row)
+    data_rows = data_rows + [pyspark.mllib.linalg.Vectors.sparse(cols, zip(data_row.indices, data_row.data))]
+rdd_rows = sc.parallelize(data_rows)
+rowmatrix = pyspark.mllib.linalg.distributed.RowMatrix(rdd_rows)
+rowmatrix.computePrincipalComponents(3)
+
+end = time.time()
+delta = end-start
+print "PCA(3)  took: ", delta, " seconds, for matrix: rw136".
+
+start = time.time()
+
+for row in range(rows):
+    data_row = mat2.getrow(row)
+    data_rows = data_rows + [pyspark.mllib.linalg.Vectors.sparse(cols, zip(data_row.indices, data_row.data))]
+rdd_rows = sc.parallelize(data_rows)
+rowmatrix = pyspark.mllib.linalg.distributed.RowMatrix(rdd_rows)
+rowmatrix.computeCovariance()
+
+end = time.time()
+delta = end-start
+print "Cov  took: ", delta, " seconds, for matrix: rw136".
+
+start = time.time()
+
+for row in range(rows):
+    data_row = mat2.getrow(row)
+    data_rows = data_rows + [pyspark.mllib.linalg.Vectors.sparse(cols, zip(data_row.indices, data_row.data))]
+rdd_rows = sc.parallelize(data_rows)
+rowmatrix = pyspark.mllib.linalg.distributed.RowMatrix(rdd_rows)
+rowmatrix.computeSVD(3)
+
+end = time.time()
+delta = end-start
+print "SVD(3)  took: ", delta, " seconds, for matrix: rw136".
