5084fc6990301669274205c7521eadcddbf643fb
Jonas Krook <jonas.krook@zenuity.com>
2019-05-28 16:56:47 +0200

    Reverts the previous commits
    Revert "another one"
    
    This reverts commit b4c4c56c904f1d6dd2580f231073df8deb4507e6#gitext://gotocommit/b4c4c56c904f1d6dd2580f231073df8deb4507e6.
    
    Revert "EVEN LARGER"
    
    This reverts commit 5da2cccd5ac56dcbd9843469ca9ec6d73441d2ce#gitext://gotocommit/5da2cccd5ac56dcbd9843469ca9ec6d73441d2ce.
    
    Revert "chnaged rocket problem to comsol"
    
    This reverts commit 142c3be393c839fc73cbef29b8f986a857faafa5#gitext://gotocommit/142c3be393c839fc73cbef29b8f986a857faafa5.
    
    Revert "code for rocket"
    
    This reverts commit 7763013c2db8a30a44e9abed20858ff4e293896d#gitext://gotocommit/7763013c2db8a30a44e9abed20858ff4e293896d.
    
    Revert "another matrix in the wall"
    
    This reverts commit 0422b884299a61449e057c381e3ae9cc246f4ea7#gitext://gotocommit/0422b884299a61449e057c381e3ae9cc246f4ea7.
    
    Contained in branches: master#gitext://gotobranch/master
    Contained in no tag

--- hannes_test.py
+++ hannes_test.py
@@ -8,7 +8,6 @@ import time
 
 mat = scipy.io.mmread('rw136.mtx')
 mat2 = scipy.io.mmread('dwt_361.mtx')
-mat3 = scipy.io.mmread('goddardRocketProblem_2.mtx')
 
 sc = pyspark.context.SparkContext.getOrCreate()
 
@@ -95,45 +94,3 @@ rowmatrix.computeSVD(3)
 end = time.time()
 delta = end-start
 print "SVD(3)  took: ", delta, " seconds, for matrix: dwt_361."
-
-(rows, cols) = mat3.shape
-data_rows = []
-
-start = time.time()
-
-for row in range(rows):
-    data_row = mat3.getrow(row)
-    data_rows = data_rows + [pyspark.mllib.linalg.Vectors.sparse(cols, zip(data_row.indices, data_row.data))]
-rdd_rows = sc.parallelize(data_rows)
-rowmatrix = pyspark.mllib.linalg.distributed.RowMatrix(rdd_rows)
-rowmatrix.computePrincipalComponents(3)
-
-end = time.time()
-delta = end-start
-print "PCA(3)  took: ", delta, " seconds, for matrix: rocketman."
-
-start = time.time()
-
-for row in range(rows):
-    data_row = mat3.getrow(row)
-    data_rows = data_rows + [pyspark.mllib.linalg.Vectors.sparse(cols, zip(data_row.indices, data_row.data))]
-rdd_rows = sc.parallelize(data_rows)
-rowmatrix = pyspark.mllib.linalg.distributed.RowMatrix(rdd_rows)
-rowmatrix.computeCovariance()
-
-end = time.time()
-delta = end-start
-print "Cov  took: ", delta, " seconds, for matrix: rocketman."
-
-start = time.time()
-
-for row in range(rows):
-    data_row = mat3.getrow(row)
-    data_rows = data_rows + [pyspark.mllib.linalg.Vectors.sparse(cols, zip(data_row.indices, data_row.data))]
-rdd_rows = sc.parallelize(data_rows)
-rowmatrix = pyspark.mllib.linalg.distributed.RowMatrix(rdd_rows)
-rowmatrix.computeSVD(3)
-
-end = time.time()
-delta = end-start
-print "SVD(3)  took: ", delta, " seconds, for matrix: rocketman."
