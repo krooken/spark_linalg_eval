05a0fb17c0792d5a2fb77ecbbaba179cc28e7daf
Jonas Krook <jonas.krook@zenuity.com>
2019-05-29 11:48:34 +0200

    Adding extra level in struct for matrix type

--- google-cloud-dataproc-metainfo/parse_output.m
+++ google-cloud-dataproc-metainfo/parse_output.m
@@ -4,11 +4,12 @@ regexpr = '(PCA\(3\)|Cov|SVD\(3\))\W*took:\W*(\d*\W?\d*)\W*seconds, for matrix: 
 
 matches = regexp(filecontents, regexpr, 'tokens');
 
-keyvalues = cell(numel(matches),2);
+keyvalues = cell(numel(matches),3);
 
 for i = 1:numel(matches)
-    keyvalues{i,1} = [matches{i}{1} '-' matches{i}{3}];
-    keyvalues{i,2} = str2double(matches{i}{2});
+    keyvalues{i,1} = matches{i}{3};
+    keyvalues{i,2} = matches{i}{1};
+    keyvalues{i,3} = str2double(matches{i}{2});
 end
 
 end
--- google-cloud-dataproc-metainfo/reduce_job_data.m
+++ google-cloud-dataproc-metainfo/reduce_job_data.m
@@ -8,15 +8,33 @@ for i = 1:numel(filenames)
     filecontents = fileread(filenames{i});
     keyvalues = parse_output(filecontents);
     for j = 1:size(keyvalues,1)
-        valid_name = get_valid_name(keyvalues{j,1});
-        if isfield(data, valid_name)
-            data.(valid_name) = [data.(valid_name) keyvalues{j,2}];
-        else
-            data.(valid_name) = [keyvalues{j,2}];
-        end
+        data = add_to_struct(data, keyvalues(j, 1:2), keyvalues{j,3});
     end
 end
 
 reduced_data = data;
+
+end
+
+function s = add_to_struct(s, fields, data)
+
+if numel(fields) > 0
+    
+    valid_name = get_valid_name(fields{1});
+    if isfield(s, valid_name)
+        s.(valid_name) = add_to_struct(s.(valid_name), fields(2:end), data);
+    else
+        s.(valid_name) = add_to_struct(struct(), fields(2:end), data);
+    end
+    
+else
+    
+    if isstruct(s)
+        s = data;
+    else
+        s = [s data];
+    end
+    
+end
 
 end
